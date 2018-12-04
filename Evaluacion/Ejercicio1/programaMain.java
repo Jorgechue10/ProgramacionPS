0ea560aa16c9438db4bb4229a9be26ade5ac4361
Jorge Chueca <jorgechue10@gmail.com>
2018-12-04 18:48:23 +0100

    Clase main que llama a las clases LeerCadena y leerDesdeFichero

--- Evaluacion/Ejercicio1/programaMain.java
+++ Evaluacion/Ejercicio1/programaMain.java
@@ -1,10 +1,19 @@
 package Ejercicio1;
 
+import java.io.IOException;
+
 public class programaMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		//Llama primero a la clase LeerCadena
+		System.out.println("LLAMANDO A LA CLASE LeerCadena....");
 		LeerCadena l=new LeerCadena();
 		l.initroducirCadena();
+		
+		System.out.println("LLAMANDO A LA CLASE leerDesdeFichero....");
+		//Después llama a la clase leerDesdeFichero
+		leerDesdeFichero f=new leerDesdeFichero();
+		f.leerFichero();
 	}	
 }
