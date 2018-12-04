4f5f65ad647e6ff3e87e5a9f0c6586af9c46b660
Jorge Chueca <jorgechue10@gmail.com>
2018-12-04 18:38:02 +0100

    Clase main, donde ejecuto la clase leerCadena

---
+++ Evaluacion/Ejercicio1/programaMain.java
@@ -0,0 +1,10 @@
+package Ejercicio1;
+
+public class programaMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LeerCadena l=new LeerCadena();
+		l.initroducirCadena();
+	}	
+}
