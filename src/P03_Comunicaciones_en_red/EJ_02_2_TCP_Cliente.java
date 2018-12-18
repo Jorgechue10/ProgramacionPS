a52faceadd0457cf25d3ab28c824b59fd2fd3277
Jorge Chueca <jorgechue10@gmail.com>
2018-12-18 18:48:50 +0100

    Ejemplo conexion CP cliente

---
+++ src/P03_Comunicaciones_en_red/EJ_02_2_TCP_Cliente.java
@@ -0,0 +1,34 @@
+package P03_Comunicaciones_en_red;
+
+import java.io.*;
+import java.net.*;
+
+public class EJ_02_2_TCP_Cliente  {
+  public static void main(String[] args) throws Exception {
+	String Host = "localhost";
+	int Puerto = 6000;//puerto remoto	
+	
+	System.out.println("PROGRAMA CLIENTE INICIADO....");
+	Socket Cliente = new Socket(Host, Puerto);
+
+	// CREO FLUJO DE SALIDA AL SERVIDOR	
+	DataOutputStream flujoSalida = new
+            DataOutputStream(Cliente.getOutputStream());
+
+	// ENVIO UN SALUDO AL SERVIDOR
+	flujoSalida.writeUTF("Saludos al SERVIDOR DESDE EL CLIENTE");
+
+	// CREO FLUJO DE ENTRADA AL SERVIDOR	
+	DataInputStream flujoEntrada = new 
+            DataInputStream(Cliente.getInputStream());
+
+	// EL SERVIDOR ME ENVIA UN MENSAJE
+	System.out.println("Recibiendo del SERVIDOR: \n\t" + 
+               flujoEntrada.readUTF());
+
+	// CERRAR STREAMS Y SOCKETS	
+	flujoEntrada.close();	
+	flujoSalida.close();	
+	Cliente.close();	
+  }// main
+}// 
