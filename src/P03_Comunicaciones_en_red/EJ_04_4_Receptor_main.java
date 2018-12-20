425f953685a402a485ccb4583f69198c38cfe339
Jorge Chueca <jorgechue10@gmail.com>
2018-12-20 18:46:04 +0100

    Ejemplo receptor multicast main

---
+++ src/P03_Comunicaciones_en_red/EJ_04_4_Receptor_main.java
@@ -0,0 +1,11 @@
+package P03_Comunicaciones_en_red;
+
+public class EJ_04_4_Receptor_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		EJ_04_2_MulticastReceptor receptor = new EJ_04_2_MulticastReceptor();
+		receptor.start();	
+	}
+}
