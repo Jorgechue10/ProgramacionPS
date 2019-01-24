2b917d0777c7de0d5dd7d599fb8404417f69cc9d
Jorge Chueca <jorgechue10@gmail.com>
2019-01-24 18:42:51 +0100

    ejemplo enviar mensaje desde java

---
+++ src/P04_Servicios_en_red/EJ_08_Envio_correo_SMTP.java
@@ -0,0 +1,52 @@
+package P04_Servicios_en_red;
+
+import java.io.IOException;
+import java.net.ConnectException;
+
+import org.apache.commons.net.smtp.SMTPClient;
+import org.apache.commons.net.smtp.SMTPReply;
+
+public class EJ_08_Envio_correo_SMTP {
+
+	public static void main(String[] args) {
+		SMTPClient client = new SMTPClient();
+		try {
+			int respuesta;
+			client.connect("localhost");
+			System.out.print(client.getReplyString());
+			respuesta = client.getReplyCode();
+
+			if (!SMTPReply.isPositiveCompletion(respuesta)) {
+				client.disconnect();
+				System.err.println("Conexión rechazada por el servidor.");
+				System.exit(1);
+			}
+
+			client.login(); // inicio de sesión HELO
+			String destinatario = "alejandro.sanesteban.pascual@iestubalcain.net";
+			String mensaje = "Hola. \nEste mensaje se envía desde un cliente Java\n";
+			String remitente = "jorge.chueca.guerra@iestubalcain.net";
+
+			if (client.sendSimpleMessage(remitente, destinatario, mensaje))
+				System.out.println("El mensaje se ha enviado a " + destinatario);
+			else
+				System.out.println("El mensaje no se ha podido enviar");
+
+			// final de sesión QUIT
+			client.logout();
+
+			client.disconnect();
+
+		}catch(ConnectException ce){
+			System.err.println("Servidor NO iniciado");			
+			System.err.println(ce.getMessage());
+			System.exit(2);
+		}
+		catch (IOException e) {
+			System.err.println("No se ha podido conectar al servidor");
+			e.printStackTrace();
+			System.exit(2);
+		}
+
+	}
+}
