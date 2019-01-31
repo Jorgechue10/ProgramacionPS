eae67beafd30e1cb3eb6d686dd505fdf36e9e40a
Jorge Chueca <jorgechue10@gmail.com>
2019-01-31 18:38:41 +0100

    Ejemplo servido_fichero

---
+++ Unidad_04_Servicios_en_red/Servidor_Ficheros/ObtieneFichero.java
@@ -0,0 +1,16 @@
+package Servidor_Ficheros;
+
+import java.io.Serializable;
+
+@SuppressWarnings("serial")
+public class ObtieneFichero implements Serializable {
+	byte[] contenidoFichero;
+
+	public ObtieneFichero(byte[] contenidoFichero) {
+		this.contenidoFichero = contenidoFichero;
+	}
+
+	public byte[] getContenidoFichero() {
+		return contenidoFichero;
+	}
+}
